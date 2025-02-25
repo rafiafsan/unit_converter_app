87659e4e1ee574918eb50a2a5313df64eef6e66c
rafiafsan <rafiafs45@gmail.com>
2025-02-26 02:04:23 +0600

    done

--- lib/presentation/ui/screen/homePage.dart
+++ lib/presentation/ui/screen/homePage.dart
@@ -8,6 +8,23 @@ class Unitconverter extends StatefulWidget {
 }
 
 class _UnitconverterState extends State<Unitconverter> {
+  TextEditingController controller = TextEditingController();
+  String fromUnit = 'Meters';
+  String toUnit = 'Kilometers';
+  double result = 0.0;
+
+  final Map<String, dynamic> convertionTypes = {
+    'Meters': 1.0,
+    'Kilometers': 0.001,
+    'Centimeters': 100,
+    'Inches': 39.37,
+    'Feet': 3.28084,
+  };
+
+  convert() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +33,51 @@ class _UnitconverterState extends State<Unitconverter> {
           'Unit Converter',
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
+      ),
+      body: Column(
+        children: [
+          TextFormField(
+            controller: controller,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              DropdownButton<String>(
+                value: fromUnit,
+                items: convertionTypes.keys.map(
+                      (x) {
+                    return DropdownMenuItem(
+                      child: Text(x),
+                      value: x,
+                    );
+                  },
+                ).toList(),
+                onChanged: (value) {
+                  fromUnit = value!;
+                },
+              ),
+              Text('to',style: TextStyle(fontSize: 17),),
+              DropdownButton<String>(
+                value: toUnit,
+                items: convertionTypes.keys.map(
+                      (x) {
+                    return DropdownMenuItem(
+                      child: Text(x),
+                      value: x,
+                    );
+                  },
+                ).toList(),
+                onChanged: (value) {
+                  fromUnit = value!;
+                },
+              ),
+            ],
+          ),
+          SizedBox(height: 10,),
+          ElevatedButton(onPressed: (){
+            convert();
+          }, child: Text('Convert'))
+        ],
       ),
     );
   }
